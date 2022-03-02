6ffc330ed2bbab242c32708a92938ac15d2ca15c
Emerge <egreengirl1@gmail.com>
2022-03-02 14:20:25 -0700

    updated file path to the generate markdown file

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./generateMarkdown')
 
 
 // GIVEN a command-line application that accepts user input
